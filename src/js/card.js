b6f182a1aa413f3727888275d59bca9afcbeb5e7
Joyce Lin <joycelin79@gmail.com>
2018-04-28 18:43:07 -0700

    styling tooltip

--- src/js/card.js
+++ src/js/card.js
@@ -28,29 +28,31 @@ class Card extends Component {
 
     render() {
 
+        console.log(this.state.forecast);
         let dayRow = this.state.forecast.map((day, index) => {
             let dayName = day.date.weekday_short;
             let minTemp = day.low.fahrenheit;
             let maxTemp = day.high.fahrenheit;
             let dayDescription = day.conditions;
             let dayIconUrl = "https://icons.wxug.com/i/c/i/" + day.icon + '.gif';
-            let dayTooltip = day.fcttext;
+            let dayTooltip = day.conditions;
 
             return (
                 <li key={index} className="day-card">
                     <a href="#day">
-                        <div className="card-contents">
+                        <div className="card-contents tooltip">
                             <p>{dayName}</p>
                             <img alt={dayDescription} src={dayIconUrl} />
+                            {/*<div className="tooltip">&#9432;*/}
+                                <span className="tooltiptext">{dayTooltip}</span>
+                            {/*</div>*/}
                         </div>
                         <ul className="temps">
                             <li className="max-temp">{maxTemp}</li>
                             <li className="min-temp">{minTemp}</li>
                         </ul>
                     </a>
-                    <div className="tooltip">&#9432;
-                        <span className="tooltiptext">{dayTooltip}</span>
-                    </div>
+
                 </li>
             )
         });
